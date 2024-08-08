92c0f5e4f8981196e4a457df8a87eb82c47a1ebc
siryancealot <2287796+scottyancey@users.noreply.github.com>
2024-08-08 09:40:44 -0700

    Fixing up server routing for SPAs

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,12 @@ async fn main() {
 fn init_router() -> Router {
     // We need to serve from the build directory itself so the relative paths are correct for 
     // the React app files.
+    let react_app_dist_location = "my-react-ts-app/build";
     Router::new()
-        // Route to our React app
-        .nest_service("/", ServeDir::new("my-react-ts-app/build").not_found_service(ServeFile::new("index.html")),)
+        // Route to our React app 
+        // Mote tha fallback file is the SPA's root index.html, so that the server knows to send all browser click 
+        // to the SPA root app (which will then route from there) vs. looking for that phyiscal file to serve at that URL.
+        .nest_service("/", ServeDir::new(react_app_dist_location).not_found_service(ServeFile::new( react_app_dist_location.to_owned() + "/index.html")),)
         // Route to a random piece of dynamic generated content (simulating an API call/response)
         .route("/rando", get(random_number_handler))
         // Route to a random static html file
@@ -37,5 +40,5 @@ async fn random_number_handler(Query(range): Query<RangeParameters>) -> Html<Str
     let random_number = thread_rng().gen_range(range.start..range.end);
 
     // Send response in html format.
-    Html(format!("<h1>Random Number: {}</h1>", random_number))
+    Html(format!("<h1>Random Number: {}</h1></br><a href='/'>Go Back</a>", random_number))
 }
