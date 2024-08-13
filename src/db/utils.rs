336e45c1359de5304b4040a40d7c26e49f91924f
siryancealot <2287796+scottyancey@users.noreply.github.com>
2024-08-12 22:41:11 -0700

    Breaking up the db connect string values

--- src/db/utils.rs
+++ src/db/utils.rs
@@ -10,7 +10,7 @@ pub async fn init_db_conn_pool() -> Result<(), sqlx::Error> {
  
     let pool = PgPoolOptions::new() 
         .max_connections(5)
-        .connect( &std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .connect( get_db_connect_string().as_str())
         .await?;
 
     let row: (i64,) = sqlx::query_as("SELECT $1")
@@ -26,6 +26,18 @@ pub async fn init_db_conn_pool() -> Result<(), sqlx::Error> {
     }
 
     Ok(())
+}
+
+// Return the DB connect string from the .env file, priting out the string with the DB password redacted.
+fn get_db_connect_string() -> String {
+
+    let password: &str = &std::env::var("DATABASE_PASSWORD").expect("DATABASE_PASSWORD must be set.");
+
+    let connect_string: String = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    let redacted_connect_string: String = connect_string.replace(password, "<password_redacted>");
+    println!("DB Connect str: {0}", redacted_connect_string);
+
+    return connect_string
 }
 
 // TODO SWY: does this just hand a DB connection as an effect of the &'static" syntax?  If so, rename.
