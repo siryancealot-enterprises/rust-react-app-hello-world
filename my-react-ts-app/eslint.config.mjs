8488dd39f78512ebfd420e90be49fb510357c089
siryancealot <2287796+scottyancey@users.noreply.github.com>
2024-08-15 22:19:27 -0700

    Updating crate/pakcage minor versions

--- my-react-ts-app/eslint.config.mjs
+++ my-react-ts-app/eslint.config.mjs
@@ -5,10 +5,10 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-//   ...tseslint.configs.recommended,
+  // ...tseslint.configs.recommended,  // this is the default, but instead we're trying the next two settings to keep things tighter (for now)
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
   {
-    ignores: ["build/*"]
-}
+      ignores: ["build/*"]
+  }
 );
