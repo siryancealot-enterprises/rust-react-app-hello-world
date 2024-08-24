0dbada6a1b88cdd0024d92fc23c71b3f96db086a
siryancealot <2287796+scottyancey@users.noreply.github.com>
2024-08-24 12:54:26 -0700

    Missing not null on player.id column

--- migrations/20240813050718_init.up.sql
+++ migrations/20240813050718_init.up.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS player
 (
-    id uuid DEFAULT gen_random_uuid(), -- Postgres defaults this to v4
+    id uuid NOT NULL DEFAULT gen_random_uuid(), -- Postgres defaults this to v4
     name varchar(32) NOT NULL,
     number integer NOT NULL,
     username varchar(32) NOT NULL,
