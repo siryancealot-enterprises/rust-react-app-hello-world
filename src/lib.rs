9e1357c1c66cb143a351a80066be8063c6932329
siryancealot <2287796+scottyancey@users.noreply.github.com>
2024-08-26 16:37:10 -0700

    Adding Crate documentation! Run from terminal: cargo doc --open

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+//! # my_rust_react_app_hello_world
+//!
+//! `my_rust_react_app_hello_world` demonstrates how to use Rust as the single server and runtime needed to serve up a React Single Page Application (SPA) written in TypeScript backed by a Postgres DB.
+//!
+//! The SPA talks to APIs on the Rust server, which talks to a Postgres DB.
+//!
+//! We do not use another server for serving the React app (such as Node.js), rather the single Rust server serves up the statically generated React application files to the requesting client. We still use Node to compile the React SPA and generate its static files.
+//!
+//! We're using Rust's [Axum](https://docs.rs/axum/latest/axum/) web application server framework.
+//!
+//! We're using Postgres DB with Rust's [sqlx](https://docs.rs/sqlx/latest/sqlx/) toolkit for client interaction and other DB utilities.
+//!
+//! We've added Rust unit and integration tests for this code.
 pub mod api;
 pub mod services;
 
